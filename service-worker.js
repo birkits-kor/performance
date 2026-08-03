6001e24a2c25f0f122373a01ad56bdad11a0ee72
hero198 <hero198@naver.com>
2026-08-03 13:54:53 +0900

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,24 @@
+const VERSION = 'birkits-dashboard-20260803-hero-month-5';
+
+self.addEventListener('install', function() {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(keys.map(function(key) {
+        return key.indexOf('birkits-dashboard-') === 0 && key !== VERSION
+          ? caches.delete(key)
+          : Promise.resolve();
+      }));
+    }).then(function() {
+      return self.clients.claim();
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  if (event.request.mode !== 'navigate') return;
+  event.respondWith(fetch(new Request(event.request, { cache: 'reload' })));
+});
